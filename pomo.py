2f59a783d00d8c4c03c904658836d012a894af93
AMAL <mailtoamaldevcd@gmail.com>
2021-06-01 22:55:45 +0530

    settings section enabled

--- pomo.py
+++ pomo.py
@@ -2,13 +2,16 @@ import time
 import chime
 
 
-print("___________________POMODORO____________________\n","1. Press 1 to start the default 25 min timer\n","2. Press two to configure the pomodoro settings\n")
+print("___________________POMODORO____________________\n","1. Press 1 to start the default 25 min timer\n","2. Press 2 to configure the pomodoro settings\n")
 s=input()
-if(s.lower()=="start"):
+if(s=='1'):
     print("Press Ctrl + C to stop pomodoro")
     time.sleep(1500)
     for i in range(1,6):
         chime.success()
         time.sleep(1)
+elif(s=='2'):
+    print("SETTINGS")
+
         
 
