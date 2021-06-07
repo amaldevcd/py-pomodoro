5800b023ad883cde56311a60b0e4a9095f9cbcea
AMAL <mailtoamaldevcd@gmail.com>
2021-06-07 10:10:17 +0530

    infinite loop

--- pomo.py
+++ pomo.py
@@ -1,15 +1,20 @@
 import time
 import chime
 
-
+t=1
 print("___________________POMODORO____________________\n","1. Press 1 to start the default 25 min timer\n","2. Press 2 to configure the pomodoro settings\n")
 s=input()
 if(s=='1'):
-    print("Press Ctrl + C to stop pomodoro")
-    time.sleep(1500)
-    for i in range(1,6):
-        chime.success()
-        time.sleep(1)
+    while t==1:
+        print("Press Ctrl + C to stop pomodoro")
+        time.sleep(1500)
+        for i in range(1,6):
+            chime.success()
+            time.sleep(1)
+        time.sleep(300)
+        for i in range(1,4):
+            chime.info()
+            time.sleep(1)
 elif(s=='2'):
     print("\n\nSETTINGS","\n\n1. Change work duration\n2. Change the short break duration\n3. Change the long break duration")
 
